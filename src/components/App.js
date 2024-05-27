237ec194b4abb0ee389d6e2787998bc06a58d18a
Nikita Bykov <nikita.bykov1995@gmail.com>
2024-05-27 20:14:41 +0300

    fixed bags

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 
 import "../styles/App.css"
 import Header from "./Header"
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "../styles/Main.css"
 
-function Main {
+function Main() {
   return (
     <main>This is Main</main>
   )
